a39c958452f2d1409151629800f3fa024adf91bb
Abdelrahman Abdelrazek <abdou007.abdelrahman@gmail.com>
2016-04-23 20:11:53 +0200

    connect4 class constructor doesnot need number of consecutive markers

--- Connect4Game.h
+++ Connect4Game.h
@@ -5,7 +5,7 @@ class Connect4Game : public BoardGame
 private:
 	bool virtual playTurn(Player p);
 public:
-	Connect4Game(int width, int height, int no_consecutive_markers_to_win, list<Player> players)
-		:BoardGame(width, height, no_consecutive_markers_to_win, players) {}
+	Connect4Game(int width, int height, list<Player> players)
+		:BoardGame(width, height, 4, players) {}
 };
 
